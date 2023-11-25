63989916d505f27eeb2f8dba7a7bd3e9bff42a1c
bokkaa <bokyh0327@gmail.com>
2023-11-25 17:27:45 +0900

    1. 관리자 페이지 사이드메뉴 동적 이동 추가. 2. 주문 페이지 추가.

--- admin/js/adminGoodsDetail.js
+++ admin/js/adminGoodsDetail.js
@@ -1,3 +1,13 @@
 $('.modify-btn').on('click', function(){
     window.location.href="/admin/html/adminGoodsModify.html";
 })
+
+
+$(document).ready(function(){
+    $('.goods-related-info-title').on('click', function(){
+
+        let add = $('.goods-related-q-container');
+        add.stop().slideToggle(400);
+
+    })
+})
--- admin/js/adminIndex.js
+++ admin/js/adminIndex.js
@@ -21,4 +21,37 @@ let myLineChart = new Chart(joinCanvas.getContext('2d'), {
 
 
 
+$(document).ready(function() {
+    let floatPosition = parseInt($(".admin-fragment").css('top'));
+
+    $(window).scroll(function() {
+    let scrollTop = $(window).scrollTop() ;
+    let attachedPosition = scrollTop + floatPosition + "px";
+
+    $(".admin-fragment").css('top', attachedPosition);
+    
+
+    }).scroll();
+
+    });
+
+$(document).ready(function(){
+    let adminHeaderHeight = $('.top-section').height();
+    console.log(adminHeaderHeight)
+
+    $('.admin-hidden-title').hide();
+
+    $(window).scroll(function(){
+        let fixedAdminTitle = $(this).scrollTop()>=adminHeaderHeight;
+
+        if(fixedAdminTitle){
+            $('.admin-hidden-title').fadeIn(100);
+        }else {
+            $('.admin-hidden-title').hide();
+
+        }
+    })
+})
+
+
 
