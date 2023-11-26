b9c4dd2b75382b9a377368aa6bc6ec131316d4a8
bokkaa <bokyh0327@gmail.com>
2023-11-26 21:38:52 +0900

    상품 리뷰 리스트 / 상품 리뷰 상세 추가.

--- admin/js/adminGoodsReviewDetail.js
+++ admin/js/adminGoodsReviewDetail.js
@@ -1,3 +1,43 @@
+//리뷰 이미지
+$('.review-imgs-btn').on('click', function(){
+
+    let imgs = $('.review-imgs img');
+  console.log(imgs);
+
+    $('.review-imgs-slide').slideToggle()
+
+    if(imgs.length == 1){
+
+    }else if(imgs.length ==2){
+
+      $('.review-imgs').css('width', '465px');
+
+    } else {
+      $('.review-imgs').css('width', '100%');
+
+    }
+
+    if($('.review-imgs-btn').text()=='리뷰 사진 열기'){
+      $('.review-imgs-btn').text('리뷰 사진 접기')
+      
+    }else {
+      $('.review-imgs-btn').text('리뷰 사진 열기')
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 //답글 수정창 팝업
 $('.goods-review-content-container').on('click', '.modify-reply-btn', function () {
 
@@ -10,7 +50,7 @@ $('.goods-review-content-container').on('click', '.modify-reply-btn', function (
       <div class='modify-box'>
         <textarea class='modify-content' cols="30" rows="2">${$content.text()}</textarea>
         <div class="modify-btn">
-        <button type='button' class='modify-content-btn'>수정 완료</button>
+        <button type='button' class='modify-content-btn btn'>수정 완료</button>
         </div>
       </div>
     
@@ -19,4 +59,9 @@ $('.goods-review-content-container').on('click', '.modify-reply-btn', function (
 
 });
 
+$('.goods-review-content-container').on('click', '.modify-content-btn', function () {
 
+    let modifyContent = $(this).closest('.if-reply').find('.modify-content').val();
+    console.log(modifyContent)
+
+})
